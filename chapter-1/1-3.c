c1257bf4e74b5f3ef6e762415b7a371193a5800f
Edna <ecao@fastly.com>
2017-06-15 16:44:30 -0700

    1-3

--- chapter-1/1-3.c
+++ chapter-1/1-3.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 
 /*print Fahrenheit-Celsius table
-   for fahr = 0, 20, ..., 300; floating-point version */
+   for fahr = 0, 20, ..., 300; floating-point version with header*/
 
 int main(void)
 {
 	float fahr, celsius;
-	int lower, upper, step;
+	int   lower, upper, step;
 
-	lower = 0;    /* lower limit of temperature table */
+	lower =   0;  /* lower limit of temperature table */
 	upper = 300;  /* upper limit */
-	step = 20;    /* step size */
+	step =   20;  /* step size */
 
 	fahr = lower;
 
 	printf("Fahrenheit\tCelsius\n");
 	
 	while (fahr <= upper) {
-		celsius = (5.0/9.0) * (fahr-32.0);
+		celsius = (5.0 / 9.0) * (fahr - 32.0);
 		printf("%10.0f\t%6.1f\n", fahr, celsius);
 		fahr = fahr + step;
 	}
