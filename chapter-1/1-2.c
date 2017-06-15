016a93c873d08afc1f2296329068d79fd804b6b0
Edna <ecao@fastly.com>
2017-06-15 14:49:27 -0700

    1-2

--- chapter-1/1-2.c
+++ chapter-1/1-2.c
@@ -5,17 +5,17 @@
 
 int main(void)
 {
-    int fahr, celsius;
-    int lower, upper, step;
+	int fahr, celsius;
+	int lower, upper, step;
 
-    lower = 0;    /* lower limit of temperature table */
-    upper = 300;  /* upper limit */
-    step = 20;    /* step size */
+	lower = 0;    /* lower limit of temperature table */
+	upper = 300;  /* upper limit */
+	step = 20;    /* step size */
 
-    fahr = lower;
-    while (fahr <= upper) {
-	celsius = 5 * (fahr-32) / 9;
-	printf("%d\t%d\n", fahr, celsius);
-	fahr = fahr + step;
-    }
+	fahr = lower;
+	while (fahr <= upper) {
+		celsius = 5 * (fahr-32) / 9;
+		printf("%d\t%d\n", fahr, celsius);
+		fahr = fahr + step;
+	}
 }
