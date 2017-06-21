a02a4e104f2b16953a11814bbef1a85968d25d62
Edna <ecao@fastly.com>
2017-06-21 04:29:15 -0700

    1-8

---
+++ chapter-1/1-8.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/* write a program to count blanks, tabs, and newlines */
+
+int main(void)
+{
+	int c, nl, t, b;
+
+	nl = 0;
+	t  = 0;
+	b  = 0;
+
+	while ((c = getchar()) != EOF)
+	{
+		if (c == '\n')
+			++nl;
+		if (c == '\t')
+			++t;
+		if (c == ' ')
+			++b;
+	}
+	printf("%d new line count\n", nl);
+	printf("%d tab count\n", t);
+	printf("%d blanks count\n", b);
+
+	return 0;
+}
