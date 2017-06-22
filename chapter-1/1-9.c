a89282bbc80fb9d2909151b7101cb2a1eb144124
Edna <ecao@fastly.com>
2017-06-22 12:44:16 -0700

    1-9

---
+++ chapter-1/1-9.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/* write a program to copy its input to its output, replacing each string of one or more blanks by a single blank */
+
+int main(void)
+{
+	int c, prevblank;
+
+	prevblank = 0;
+
+	while ((c = getchar()) != EOF)
+	{	
+		if (c == ' ')
+		{
+			if (prevblank == 0)
+				putchar(c);
+				prevblank = 1;
+		}
+		if (c != ' ')
+		{
+			putchar(c);
+			prevblank = 0;
+		}
+	}
+
+	return 0;
+}
