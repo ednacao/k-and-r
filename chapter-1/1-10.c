950fa7b58bb7760039703459b37a090e6bf64c98
Edna <ecao@fastly.com>
2017-06-27 02:42:07 -0700

    1-10

---
+++ chapter-1/1-10.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/* write a program to copy its input to output, replacing each tab by \t, each backspace by \b, and each backslash by \\ */
+
+int main(void)
+{
+	int c;
+
+	while ((c = getchar()) != EOF)
+	{
+		if (c == '\t')
+			printf("\\t");
+		else if (c == '\b')
+			printf("\\b");
+		else if (c == '\\')
+			printf("\\\\");
+	}
+
+	return 0;
+}
