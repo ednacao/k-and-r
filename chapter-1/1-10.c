32676c97c0f32003be28c45726765ad512d895af
Edna <ecao@fastly.com>
2017-06-29 14:58:28 -0700

    1-10

--- chapter-1/1-10.c
+++ chapter-1/1-10.c
@@ -1,6 +1,6 @@
-#include <stdio.h>
-
 /* write a program to copy its input to output, replacing each tab by \t, each backspace by \b, and each backslash by \\ */
+
+#include <stdio.h>
 
 int main(void)
 {
