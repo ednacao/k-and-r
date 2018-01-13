18b696ec8f7639f8621fabcb97a5859996a762e8
Edna <ecao@fastly.com>
2018-01-12 17:16:45 -0800

    1-23, state tracking correctly

---
+++ chapter-1/1-23.c
@@ -0,0 +1,88 @@
+/* Write a program to remove all comments from a C program.
+ * Don't forget to handle quoted strings and character
+ * constants properly. C comments do not nest. 
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+  int c, state;
+  
+  state = 0;
+  
+/* 
+ * state 0: not inside a comment or string / reached end of comment
+ * state 1: possibly beginning of a comment
+ * state 2: inside of a comment
+ * state 4: possibly exiting a comment
+ * state 6: inside of a string
+ * state 7: possible end of string / escape sequence
+ * state 8: possible end of string / escape sequence
+ * state 10: reached end of string
+ */
+
+  while ((c = getchar()) !=EOF) 
+  {      
+      if (state == 0) {
+        if (c == '/') {
+            state = 1;
+        } else if (c == '"')  {
+            state = 6;
+        } else {
+            state = 0;
+        }
+      
+      } else if (state == 1)  {
+        if (c == '*') {
+            state = 2;
+        } else {
+            state = 0;
+        }
+        
+      } else if (state == 2)  {
+        if (c == '*') {
+            state = 4;
+        } else {
+            state = 2;
+        }
+        
+      } else if (state == 4)  {
+        if (c == '/') {
+            state = 0;
+        } else {
+            state = 2;
+        }
+      } else if (state == 6)  {
+        if (c == '"') {
+            state = 10;
+        } else if (c == '\\') {
+            state = 7;
+        } else {
+            state = 6;
+        }
+      } else if (state == 7)  {
+        if (c == '"') {
+            state = 7;
+        } else if (c == '\\') {
+            state = 8;
+        } else {
+            state = 6;
+        }
+      } else if (state == 8)  {
+        if (c == '"') {
+            state = 10;
+        }
+      } else if (state == 10) {
+        if (c == '\\') {
+            state = 1;
+        } else {
+            state = 0;
+        }
+      }
+      
+  printf("%c%d\n", c, state);
+  }
+  
+  return 0;
+}
